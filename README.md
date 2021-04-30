ce7f11c418e5abf77108e34e2d0eb0d7b1cf2a48
Patrick Wells <pwells@ucdavis.edu>
2021-04-30 16:24:12 -0700

    Reorganize

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a set of tools that are being used in the TDCOSMO collaboration to
+study the environments around strong gravitational lenses.
+
+This code is still very much under development. If you would like to install it,
+it is recommended you do so with pip in development mode.
+
+You can do this by cloning this archive, and running `pip install -e .` from the
+directory with the setup.py file. 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lenskappa",
+    version="0.0.1",
+    author="Patrick Wells",
+    author_email="pwells@ucdavis.edu",
+    description="Tools for studying the environment of strong lens systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PatrickRWells/lenskappa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
